21db5a680cbd1a9474f2671d7f368ba14785a9fa
loganewhaley <89420796+loganewhaley@users.noreply.github.com>
2024-12-02 02:01:34 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "1048302203364061184"; // your league ID
-export const leagueName = "UU Keeper League"; // your league name
+export const leagueName = "Playing for Keepers"; // your league name
 export const dues = 0; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
